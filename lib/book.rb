1bfde14d6fba9442f11acb2e712c3ef702fea56d
github username <github email address>
2018-08-29 13:13:56 -0400

    Defined class with accessor macros.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  def initialize(title, author="John Doe", page_count="100")
+    @title = title
+    @author = author
+    @page_count = page_count
+  end
 
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
