ccc325f180c4b16adb00e39d09b9d2feb2b0e528
DeCode2018 <decode2018@gmail.com>
2018-08-30 18:05:08 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
-# Make your shoe class here!
+# Shoe
+#   ::new
+#     gets initialized with a brand
+#   properties
+#     has a brand (FAILED - 8)
+#     has a color (FAILED - 9)
+#     has a size (FAILED - 10)
+#     has a material (FAILED - 11)
+#     has a condition (FAILED - 12)
+#   #cobble
+#     says that the shoe has been repaired (FAILED - 13)
+#     makes the shoe's condition new (FAILED - 14)
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts 'Your shoe is as good as new!'
+    @condition = 'new'
+  end
+end
